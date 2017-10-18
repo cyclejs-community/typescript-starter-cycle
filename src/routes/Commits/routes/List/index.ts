65c22b5e9025589be254f18c5d0653bf66cf3504
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-18 22:08:36 +0530

    Load commits list

--- src/routes/Commits/routes/List/index.ts
+++ src/routes/Commits/routes/List/index.ts
@@ -1,10 +1,27 @@
 import { RouteComponent } from 'routes';
 import { Stream } from 'xstream';
-import { h2 } from '@cycle/dom';
+import { div, h2, ul, li } from '@cycle/dom';
+import { Commit } from 'drivers/github';
 
 const xs = Stream;
 
-export const List: RouteComponent = sources => ({
-  dom: xs.of(h2(['List'])),
-  history: xs.empty()
-});
+export const List: RouteComponent = ({ dom, history, github }) => {
+  const commits$ = github.commits();
+  const commitListItems$ =
+    commits$.map(commits =>
+      commits.map(commit => li({ attrs: { 'data-sha': commit.sha } }, [commit.commit.message]))
+    );
+  const vdom$ = commitListItems$.map(commits =>
+    div([
+      h2('Commits List'),
+      ul(commits)
+    ])
+  );
+  const navigateTo$ = xs.empty();
+  const request$ = xs.of('');
+  return {
+    dom: vdom$,
+    history: navigateTo$,
+    github: request$
+  };
+};

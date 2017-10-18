85bf32c636e91bb4f2ac78903b5cf9cce8673ce4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-18 17:52:11 +0530

    Simplify layout resolution logic

--- src/components/App.ts
+++ src/components/App.ts
@@ -17,30 +17,23 @@ export interface Sinks {
 const xs = Stream;
 
 export const App = (sources: Sources): Sinks => {
-  const resolution$ =
-    sources.history
-      .map(location => resolve(location.pathname));
   const app$ =
-    xs.merge(
-      resolution$
-        .filter(resolution => !!resolution.getLayout)
-        .map(({ getComponent, getLayout, ...resolution }) =>
-          xs.fromPromise(Promise.all([getComponent(), getLayout()]))
-            .map(([Component, Layout]) => {
-              const component = Component({ ...sources, ...(resolution.sources || {}) });
-              const layoutSources = { ...sources, component };
-              return Layout(layoutSources);
-            })
-        ),
-      resolution$
-        .filter(resolution => !resolution.getLayout)
-        .map(({ getComponent, ...resolution }) =>
-          xs.fromPromise(getComponent().then(Component => Component({ ...sources, ...(resolution.sources || {}) })))
+    sources.history
+      .map(location => resolve(location.pathname))
+      .map(({ getComponent, getLayout, ...resolution }) =>
+        xs.fromPromise(
+          getComponent()
+            .then(Component => Component({ ...sources, ...(resolution.sources || {}) }))
+            .then(component =>
+              !getLayout
+                ? component
+                : getLayout().then(Layout => Layout({ ...sources, component }))
+          )
         )
-    )
-    .flatten();
+      )
+      .flatten();
   return {
     dom: pluck(app$, app$ => app$.dom),
     history: pluck(app$, app$ => app$.history)
-  }
+  };
 };

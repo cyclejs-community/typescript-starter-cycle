b83e0e5f38f5f55bd2f39cf2269a794f13746c21
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-18 18:21:35 +0530

    Add about route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -19,13 +19,27 @@ export interface RouteDefinitions {
   [path: string]: RouteResolution<any>;
 }
 
+// Example of async layout loading
+const getHeaderLayout = async () => {
+  const { HeaderLayout } = await import(/* webpackChunkName: "HeaderLayout" */'layouts/HeaderLayout');
+  return HeaderLayout;
+};
+
+// Helper to directly load components and layouts
+const getComponent = <T>(component: T) => async () => await Promise.resolve(component);
+
 const routes: RouteDefinitions = {
   '/': {
-    getComponent: async () => await Promise.resolve(Home),
-    getLayout: async () => {
-      const { HeaderLayout } = await import(/* webpackChunkName: "HeaderLayout" */'layouts/HeaderLayout');
-      return HeaderLayout;
-    }
+    getComponent: getComponent(Home),
+    getLayout: getHeaderLayout
+  },
+  '/about': {
+    // Example of async component loading
+    getComponent: async () => {
+      const { About } = await import(/* webpackChunkName: "About" */'./About');
+      return About;
+    },
+    getLayout: getHeaderLayout
   }
 };
 

381ed7e1c1b903a5a08d0dd5370b74a2d97340d3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-18 16:04:11 +0530

    Fix dist path

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ var GLOBALS = {
 
 // Constants
 var ROOT = path.resolve(__dirname);
-var DIST = path.join(ROOT, 'docs');
+var DIST = path.join(ROOT, 'dist');
 var SRC = path.join(ROOT, 'src');
 var PROJECT_PUBLIC_PATH = '/';
 

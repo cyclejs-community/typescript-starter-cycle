f0f2f43d616275272bc7ebe5b278ffaa831ad8a0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-10-18 16:10:38 +0530

    Add history

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { run } from '@cycle/run';
 import { App } from './components/App';
 import { makeDOMDriver } from '@cycle/dom';
+import { makeHashHistoryDriver } from '@cycle/history';
 
 run(App, {
-  dom: makeDOMDriver('#app')
+  dom: makeDOMDriver('#app'),
+  history: makeHashHistoryDriver()
 });
--- src/components/App.ts
+++ src/components/App.ts
@@ -1,18 +1,22 @@
-import { Stream } from 'xstream';
+import { Stream, MemoryStream } from 'xstream';
 import { DOMSource, VNode, div } from '@cycle/dom';
+import { HistoryInput, Location } from '@cycle/history';
 
 export interface Sources {
   dom: DOMSource;
+  history: MemoryStream<Location>;
 }
 
 export interface Sinks {
   dom: Stream<VNode>;
+  history: Stream<HistoryInput | string>;
 }
 
 const xs = Stream;
 
 export const App = (sources: Sources): Sinks => {
   return {
-    dom: xs.of(div('.app'))
+    dom: xs.of(div('.app', ['Hello from Cycle.js!'])),
+    history: xs.of('')
   };
 };
